c4fa331c522ac4dbe7ce9c511648a78b54c014ea
Abhinaw Tiwari <abhinawtiwari@Abhinaws-MacBook-Pro.local>
2022-11-23 16:12:56 +0000

    send interest improve

--- send-interest.py
+++ send-interest.py
@@ -37,19 +37,41 @@ def sendInterest(interest):
                 print("An exception occured")
 
 def main():
-    # interest_packets = ['car/speed', 'car/proximity', 'car/light-on', 'car/wiper-on', 'car/passengers-count', 'car/fuel', 'car/engine-temperature', 
-    # 'bike/speed', 'bike/proximity', 'bike/light-on', 'bike/wiper-on', 'bike/passengers-count', 'bike/fuel', 'bike/engine-temperature', 'truck_speed', 'truck/proximity', 'truck/light-on', 'truck/wiper-on', 'truck/passengers-count', 'truck/fuel', 'truck/engine-temperature']
-    interest_packets = ['truck/speed', 'truck/proximity', 'truck/light-on', 'truck/wiper-on', 'truck/passengers-count', 'truck/fuel', 'truck/engine-temperature',
-                    'bike/speed', 'bike/proximity', 'bike/light-on', 'bike/wiper-on', 'bike/passengers-count', 'bike/fuel', 'bike/engine-temperature']
-
+    truck_interest_packets = ['truck/speed', 'truck/proximity', 'truck/light-on', 'truck/wiper-on', 'truck/passengers-count', 'truck/fuel', 'truck/engine-temperature']
+    bike_interest_packets = ['bike/speed', 'bike/proximity', 'bike/light-on', 'bike/wiper-on', 'bike/passengers-count', 'bike/fuel', 'bike/engine-temperature']
+    car_interest_packets = ['car/speed', 'car/proximity', 'car/light-on', 'car/wiper-on', 'car/passengers-count', 'car/fuel', 'car/engine-temperature']
+    
     while True:
-        for c in interest_packets:
-            # time.sleep(10)
-            print('Press enter to send next interest packet')
-            input()
-            sendInterest(c)
-            print('\n')
-        # time.sleep(20)
+        print('\n')
+        print('Press 1 to send truck interest packets')
+        print('Press 2 to send bike interest packets')
+        print('Press 3 to send car interest packets\n')
+        val = input()
+
+        if val == '1':
+            for c in truck_interest_packets:
+                print('press 1 to send next packet, press 2 to change vehicle type')
+                inp = input()
+                if inp != '1':
+                    break
+                sendInterest(c)
+                print('\n')
+        elif val == '2':
+            for c in bike_interest_packets:
+                print('press 1 to send next packet, press 2 to change vehicle type')
+                inp = input()
+                if inp != '1':
+                    break
+                sendInterest(c)
+                print('\n')
+        elif val == '3':
+            for c in car_interest_packets:
+                print('press 1 to send next packet, press 2 to change vehicle type')
+                inp = input()
+                if inp != '1':
+                    break
+                sendInterest(c)
+                print('\n')
 
 if __name__ == '__main__':
     main()
