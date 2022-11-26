773a2229ba0ad5bd532f53526fde78347c2fa2ee
geniusAhua <648930239@qq.com>
2022-11-26 17:10:05 +0000

    ignore

--- NDN_Base_Layer/temp.py
+++ NDN_Base_Layer/temp.py
@@ -124,7 +124,7 @@ class Demo():
 
     def __broadcast(self):
         try:
-            broad = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UCD)
+            broad = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
             broad.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             broad.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
             broad.settimeout(0.5)
