8f5a40be0d0abf943d146bdbe2fe81e1100776ee
AsishRath31 <102935318+AsishRath31@users.noreply.github.com>
2022-11-27 21:49:02 +0000

    updated

--- advertise_sensor.py
+++ advertise_sensor.py
@@ -1,10 +1,22 @@
 import socket
+import threading
 import time
 import base64
-import random
+import random 
+import traceback
 
-ROUTER_IP = '10.35.70.28'
-ROUTER_PORT = 33310
+PEER_PORT = 33301    # Port for listening to other peers
+
+ROUTER_HOST = '10.35.70.28'
+ROUTER_PORT = 33334
+
+#vehicle_type = 'car' # bike, truck possible
+
+features_dict = {
+    "truck_advertise_string": '[truck/speed, truck/proximity, truck/pressure, truck/light-on, truck/wiper-on, truck/passengers-count, truck/fuel, truck/engine-temperature]',
+    "car_advertise_string": '[car/speed, car/proximity, car/pressure, car/light-on, car/wiper-on, car/passengers-count, car/fuel, car/engine-temperature]',
+    "bike_advertise_string": '[bike/speed, bike/proximity, bike/pressure, bike/light-on, bike/wiper-on, bike/passengers-count, bike/fuel, bike/engine-temperature]'
+}
 
 def bencode(toEncode):
     ascii_encoded = toEncode.encode("ascii")
@@ -18,307 +30,137 @@ def bdecode(toDecode):
     sample_string = sample_string_bytes.decode("ascii")
     return sample_string
 
-vehicleStatus={}
-vehicleStatus2={}
-vehicleStatus3={}
+def sendAck(conn, raddr, result):
+        try:
+            msg = result
+            conn.send(bencode(msg).encode())
+        except Exception:
+            print(traceback.format_exc())
+            print('exception occurred while sending acknowledgement: ', Exception)
 
+def senseSpeed():
+    baseSpeed = 80
+    randomMix = random.randint(-10, 10)
+    res = baseSpeed + randomMix
+    return str(res)
 
+def senseProximity():
+    baseProximity = 20
+    randomMix = random.randint(-10, 10)
+    res = baseProximity + randomMix
+    return str(res)
 
-#DESERT HEALTH CHECK USE CASE
-def actuate(interest_packet, data_packet):
-    
+def sensePressure():
+    if vehicle_type == 'car':
+        val = random.randint(30, 33) # car tyre pressure
+    elif vehicle_type == 'bike':
+        val = random.randint(80, 130) # two wheeler tyre pressure
+    else:
+        val = random.randint(80, 131) # truck tyre pressure
+    return str(val)
 
-   # print("Data packet received: ", data_packet, ' for interest packet ', interest_packet)
- 
-       
-   if interest_packet == 'truck/speed':
-       vehicleStatus['vehicletype']=1
-       feature = interest_packet.split("/")[1]
-       vehicleStatus[feature]=int(data_packet)
-       data_packet_int = int(data_packet)
-       if data_packet_int > 40:
-         print('WARNING: Truck approaching at speed ', data_packet ,' km/h Please slow down for vehicle health checkup before long run :)')
-       else:
-          print('Stop for Truck Health Checkup, Current Speed: ', data_packet)
-            
-   elif interest_packet == 'truck/proximity':
-        feature = interest_packet.split("/")[1]
-        vehicleStatus[feature]=int(data_packet)
-        print('Truck is  ', data_packet ,'metres away from last garage/gas station')
-        
-   elif interest_packet == 'truck/pressure':
-     feature = interest_packet.split("/")[1]
-     vehicleStatus[feature]=int(data_packet)
-     data_packet_int = int(data_packet)
-     if data_packet_int < 125:
-       print('WARNING: Truck tyre pressure is low ( ', data_packet,' psi) , get your tyre some air ----\n *STOP AT THE GARAGE(Truck)*:)')
-     else:
-       print ('Truck tyre pressure is in good condition')
-            
-   elif interest_packet == 'truck/light-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus[feature]=0
-       print("Truck Light is on")
-     elif data_packet=='off':
-       vehicleStatus[feature]=1
-       print("Truck Light is off")
-     else:
-       vehicleStatus[feature]=2
-       print("WARNING: Truck Light is faulty")
-          
-          
-   elif interest_packet == 'truck/wiper-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus[feature]=0
-       print('Truck wiper is ', data_packet)
-     elif data_packet=='off':
-       vehicleStatus[feature]=1
-       print('Truck wiper is ', data_packet)
-     else:
-       vehicleStatus[feature]=2
-       print('WARNING: Truck wiper is ', data_packet)
-            
-        
-   elif interest_packet == 'truck/passengers-count':
-     feature = interest_packet.split("/")[1]
-     vehicleStatus[feature]=int(data_packet)
-     print('Truck is approaching with ', data_packet, ' number of passengers')
-      
-   elif interest_packet == 'truck/fuel':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='low':
-       vehicleStatus[feature]=0
-       print('WARNING: this is the last gas station, Truck fuel level: ', data_packet.upper(),' Kindly refill your fuel tank')
-     elif data_packet=='medium':
-       vehicleStatus[feature]=1
-       print('WARNING: this is the last gas station, Truck fuel level: ', data_packet.upper() ,' Kindly refill your fuel tank')
-     else:
-       vehicleStatus[feature]=2
-       print('truck fuel is ',data_packet.upper(), ':Good to go')
-        
-   elif interest_packet == 'truck/engine-temperature':
-     feature = interest_packet.split("/")[1]
-     vehicleStatus[feature]=int(data_packet)
-     data_packet_int=int(data_packet)
-     if int(data_packet_int) > 200:
-       print('WARNING: Truck engine is heating up, current engine temperature: ', data_packet,' degree Celcius, Please head towards garage(Truck) before long run :)')
-     else:
-       print('Truck engine teperature is normal')
-#--------------------------------------------------------------------------------------------------------------------------
-   elif interest_packet == 'car/speed':
-       vehicleStatus2['vehicletype']=2
-       feature = interest_packet.split("/")[1]
-       vehicleStatus2[feature]=int(data_packet)
-       data_packet_int = int(data_packet)
-       if data_packet_int > 40:
-         print('WARNING: Car approaching at speed ', data_packet ,' km/h Please slow down for vehicle health checkup before long run :)')
-       else:
-          print('Stop for Car Health Checkup, Current Speed: ', data_packet)
-            
-   elif interest_packet == 'car/proximity':
-       feature = interest_packet.split("/")[1]
-       vehicleStatus2[feature]=int(data_packet)
-       print('Car is ', data_packet ,' metres away from last garage/gas station')
-        
-   elif interest_packet == 'car/pressure':
-      feature = interest_packet.split("/")[1]
-      vehicleStatus2[feature]=int(data_packet)
-      data_packet_int = int(data_packet)
-      if data_packet_int < 32:
-        print('WARNING: Car tyre pressure is LOW ( ', data_packet,' psi) , get your tyre some air ----\n *STOP AT THE GARAGE(Car)*:)')
-      else:
-        print ('Car tyre pressure is in good condition')
-        
-   elif interest_packet == 'car/light-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus2[feature]=0
-       print("Car Light is on")
-     elif data_packet=='off':
-       vehicleStatus2[feature]=1
-       print("Car Light is off")
-     else:
-       vehicleStatus2[feature]=2
-       print("WARNING: Car Light is faulty")
-          
-             
-   elif interest_packet == 'car/wiper-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus2[feature]=0
-       print('Car wiper is ', data_packet)
-     elif data_packet=='off':
-       vehicleStatus2[feature]=1
-       print('Car wiper is ', data_packet)
-     else:
-       vehicleStatus2[feature]=2
-       print('WARNING: Car wiper is ', data_packet)
-        
-   elif interest_packet == 'car/passengers-count':
-     feature = interest_packet.split("/")[1]
-     vehicleStatus2[feature]=int(data_packet)
-     print('Car is approaching with ', data_packet, ' number of passengers')
-        
-   elif interest_packet == 'car/fuel':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='low':
-       vehicleStatus2[feature]=0
-       print('WARNING: this is the last gas station, Car fuel level: ', data_packet.upper(),' Kindly refill your fuel tank')
-     elif data_packet=='medium':
-       vehicleStatus2[feature]=1
-       print('WARNING: this is the last gas station, Car fuel level: ', data_packet.upper() ,' Kindly refill your fuel tank')
-     else:
-       vehicleStatus2[feature]=2
-       print('Car fuel is ',data_packet.upper(), ':Good to go')
-        
-   elif interest_packet == 'car/engine-temperature':
-       feature = interest_packet.split("/")[1]
-       vehicleStatus2[feature]=int(data_packet)
-       data_packet_int=int(data_packet)
-       if data_packet_int > 200:
-        print('WARNING: Car engine is heating up, current engine temperature: ', data_packet,'degree Celcius, Please head towards garage(Car) before long run :)')
-       else:
-        print('Car engine temperature is normal:',data_packet_int,' degree celcius')
+def senseLight():
+    state = ['on', 'off', 'faulty']
+    return random.choice(state)
 
-#--------------------------------------------------------------------------------------------------------------------------------------
-#--------------------------------------------------------------------------------------------------------------------------------------
+def senseWiper():
+    state = ['off', 'on', 'faulty']
+    return random.choice(state)
 
-   elif interest_packet == 'bike/speed':
-       vehicleStatus3['vehicletype']=3
-       feature = interest_packet.split("/")[1]
-       vehicleStatus3[feature]=int(data_packet)
-       data_packet_int = int(data_packet)
-       if data_packet_int > 40:
-         print('WARNING: Bike approaching at speed ', data_packet ,' km/h Please slow down for vehicle health checkup before long run :)')
-       else:
-          print('Stop for Bike Health Checkup, Current Speed: ', data_packet)
-            
-   elif interest_packet == 'bike/proximity':
-       feature = interest_packet.split("/")[1]
-       vehicleStatus3[feature]=int(data_packet)
-       print('Bike is ', data_packet ,' metres away from last garage/gas station')
-        
-   elif interest_packet == 'bike/pressure':
-      feature = interest_packet.split("/")[1]
-      vehicleStatus3[feature]=int(data_packet)
-      data_packet_int = int(data_packet)
-      if data_packet_int < 105:
-        print('WARNING: Bike tyre pressure is LOW (', data_packet,' psi) , get your tyre some air ----\n *STOP AT THE GARAGE(Bike)*:)')
-      else:
-        print ('Bike tyre pressure is in good condition')
-        
-   elif interest_packet == 'bike/light-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus3[feature]=0
-       print("Bike Light is on")
-     elif data_packet=='off':
-       vehicleStatus3[feature]=1
-       print("Bike Light is off")
-     else:
-       vehicleStatus3[feature]=2
-       print("WARNING: Bike Light is faulty")
-          
-             
-   elif interest_packet == 'bike/wiper-on':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='on':
-       vehicleStatus3[feature]=0
-       print('Bike wiper is ', data_packet)
-     elif data_packet=='off':
-       vehicleStatus3[feature]=1
-       print('Bike wiper is ', data_packet)
-     else:
-       vehicleStatus3[feature]=2
-       print('WARNING: Bike wiper is ', data_packet)
-        
-   elif interest_packet == 'bike/passengers-count':
-     feature = interest_packet.split("/")[1]
-     vehicleStatus3[feature]=int(data_packet)
-     print('Bike is approaching with ', data_packet, ' number of passengers')
-        
-   elif interest_packet == 'bike/fuel':
-     feature = interest_packet.split("/")[1]
-     if data_packet=='low':
-       vehicleStatus3[feature]=0
-       print('WARNING: this is the last gas station, Bike fuel level: ', data_packet.upper(),' Kindly refill your fuel tank')
-     elif data_packet=='medium':
-       vehicleStatus3[feature]=1
-       print('WARNING: this is the last gas station, Bike fuel level: ', data_packet.upper() ,' Kindly refill your fuel tank')
-     else:
-       vehicleStatus3[feature]=2
-       print('Bike fuel is ',data_packet.upper(), ':Good to go')
-        
-   elif interest_packet == 'bike/engine-temperature':
-       feature = interest_packet.split("/")[1]
-       vehicleStatus3[feature]=int(data_packet)
-       data_packet_int=int(data_packet)
-       if data_packet_int > 200:
-        print('WARNING: Bike engine is heating up, current engine temperature: ', data_packet,'degree Celcius, Please head towards garage(Bike) before long run :)')
-       else:
-        print('Bike engine temperature is normal:',data_packet_int,' degree celcius')
+def sensePassengerCount():
+    if vehicle_type == 'car':
+        val = random.randint(1, 6)
+    else: # bike, truck
+        val = random.randint(1, 3)
+    return str(val)
 
-#--------------------------------------------------------------------------------------------------------------------------------------
+def senseFuel():
+    state = ['low', 'medium', 'full']
+    return random.choice(state)
 
-def sendInterest(interest):
-        routers = {(ROUTER_IP, ROUTER_PORT)}
-        print('attempting to send interest packet: ', interest)
-                
-        for router in routers:
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect(router)
-                base64encoded = str(bencode(interest))
-                s.send(base64encoded.encode())
-                ack = s.recv(1024)
-                print("WELCOME TO SAHARA DESSERT HIGHWAY,NO GARAGE/FUEL STATION FOR NEXT 150 KM \n ---------------------------------- \n VEHICLE HEALTH CHECKUP REPORT")
-                actuate(interest, bdecode(ack.decode('utf-8')))
-                s.close()
-            except Exception:
-                print("An exception occured")
+def senseEngineTemperature():
+    baseTemp = 200
+    randomMix = random.randint(-5, 20)
+    res = baseTemp + randomMix
+    return str(res)
+
+def callActuator(interest):
+    if interest.lower() == "speed":
+        return senseSpeed()
+    elif interest.lower() == "proximity":
+        return senseProximity()
+    elif interest.lower() == "pressure":
+        return sensePressure()
+    elif interest.lower() == "light-on":
+        return senseLight()
+    elif interest.lower() == "wiper-on":
+        return senseWiper()
+    elif interest.lower() == "passengers-count":
+        return sensePassengerCount()
+    elif interest.lower() == "fuel":
+        return senseFuel()
+    elif interest.lower() == "engine-temperature":
+        return senseEngineTemperature()
+
+class Peer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.peers = set()
+
+    def advertiseFeature(self):
+        """Advertise the host IP."""
+        server_tup = (ROUTER_HOST, ROUTER_PORT)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(server_tup)
+        dynamic_advertise_string = vehicle_type.lower() + '_advertise_string'
+        adv = features_dict[dynamic_advertise_string]
+        message = f'HOST {self.host} PORT {self.port} ACTION {adv}'
+        print('Advertising ', message)
+        s.send(message.encode())
+        s.close()
+
+def receiveData():
+    print("listening for actuation requests")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    hostname = socket.gethostname()
+    host = socket.gethostbyname(hostname)
+    port = PEER_PORT
+    s.bind((host, port))
+    s.listen(5)
+    while True:
+        conn, addr = s.accept()
+        print("addr: ", addr[0])
+        # print("connection: ", str(conn))
+        data = conn.recv(1024)
+        data = bdecode(data.decode())
+        print(data, " to actuate on")
+        # call actuators
+        [vt, interest] = data.split('/')
+        if vehicle_type == vt:
+            actuationResult = callActuator(interest)
+        
+        sendAck(conn, addr[0], actuationResult)
+        conn.close()
+        time.sleep(1)
 
 def main():
-    truck_interest_packets = ['truck/speed', 'interest/corrupted', 'truck/proximity', 'truck/pressure', 'truck/light-on', 'truck/wiper-on', 'truck/passengers-count', 'truck/fuel', 'truck/engine-temperature']
-    bike_interest_packets = ['bike/speed', 'interest/corrupted', 'bike/proximity', 'bike/pressure', 'bike/light-on', 'bike/wiper-on', 'bike/passengers-count', 'bike/fuel', 'bike/engine-temperature']
-    car_interest_packets = ['car/speed', 'interest/corrupted', 'car/proximity', 'car/pressure', 'car/light-on', 'car/wiper-on', 'car/passengers-count', 'car/fuel', 'car/engine-temperature']
-    
-    while True:
-        print('\n')
-        print('Press 1 to send truck interest packets')
-        print('Press 2 to send car interest packets')
-        print('Press 3 to send bike interest packets\n')
-        val = input()
+    hostname = socket.gethostname()
+    host = socket.gethostbyname(hostname)
+    peer = Peer(host, PEER_PORT)
 
-        if val == '1':
-            for c in truck_interest_packets:
-                print('press 1 to send next packet, press 2 to change vehicle type')
-                inp = input()
-                if inp != '1':
-                    break
-                sendInterest(c)
-                print(vehicleStatus)
-                print('\n')
-        elif val == '2':
-            for c in car_interest_packets:
-                print('press 1 to send next packet, press 2 to change vehicle type')
-                inp = input()
-                if inp != '1':
-                    break
-                sendInterest(c)
-                print(vehicleStatus2)
-                print('\n')
-        elif val == '3':
-            for c in bike_interest_packets:
-                print('press 1 to send next packet, press 2 to change vehicle type')
-                inp = input()
-                if inp != '1':
-                    break
-                sendInterest(c)
-                print(vehicleStatus3)
-                print('\n')
+    print('\n')
+    print('Enter vehicle type (truck/car/bike): ')
+    val = input()
+    global vehicle_type
+    vehicle_type = val
+        
+    while True:
+        t1 = threading.Thread(target=peer.advertiseFeature)
+        t1.start()
+        time.sleep(10)
+        receiveData()
 
 if __name__ == '__main__':
     main()
-
